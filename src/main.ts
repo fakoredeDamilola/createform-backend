549b3e98bd712cb33dab278a0c82fdffef16ca97
Fakorede <dammy.fakorede@gmail.com>
2025-03-10 12:55:08 +0100

    temporaily allow any access

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://createform-neon.vercel.app/'],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3333);
 }
